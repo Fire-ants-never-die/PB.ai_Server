ca219b7fa0bcb08bba860e7febaf78301e9775f7
jinuchoi <jinu010219@naver.com>
2025-10-30 15:39:45 +0900

    .

--- program_tool.py
+++ program_tool.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 # decorator
 def singleton(cls):
@@ -16,6 +17,26 @@ def logging(func):
         print(f"{func.__name__} finished")
     return wrapper
 
+@singleton
+class DateTimeManager():
+
+    def __init__(self):
+        self.set_time()
+
+    def set_time(self):
+        self.now = datetime.datetime.now()
+        #format
+        #ex 20201231
+        self.formatted_today = self.now.strftime("%Y%m%d")
+        self.formatted_year = self.now.strftime("%Y")
+    
+    #return formatted_today - date 
+    def get_past_time(self,date:int) -> str:
+        past_time = self.now - datetime.timedelta(days=date)
+        formatted_past_time = self.now.strftime("Y%m%d")
+        return formatted_past_time
+
+
 #debuger
 class Debuger():
     @staticmethod
@@ -25,3 +46,19 @@ class Debuger():
     def printc(msg):
         os.system("cls")
         print(f"[DBG] {msg}",flush = True)
+
+class LoadingDebugger(Debuger):
+    def __init__(self,name,total):
+        os.system("cls")
+        super().printd(f"{name} 시작")
+        self.name = name
+        self.total = total
+        self.progress = 0.0
+    def print_progress(self,cnt,flush = False):
+        if cnt / self.total >= self.progress:
+            if flush:
+                os.system("cls")
+            super().printd(f"{self.name} 진행률 : {cnt}/{self.total}   ||  {cnt/self.total * 100}%")
+            self.progress += 0.1
+    def __del__(self):
+            super().printd(f"{self.name} 완료")
--- report_crawler.py
+++ report_crawler.py
@@ -1,42 +1,71 @@
+import pandas as pd
+import json
 from program_tool import *
 from data_controller import DataController
+from tqdm import tqdm
 import OpenDartReader
+import openpyxl
+import pyarrow
 
 @singleton
 class ReportCrawler():
 
-    def __init__(self,data_controller : DataController):
+    def __init__(self):
+        #api key등이 담긴 data/meta.json 
+        meta_data = DataController().get_meta_data()
+        self.api_key = meta_data["api_key"]
 
-        #get api key
-        self.data_controller = data_controller
-        self.meta_data = data_controller.get_meta_data()
-        self.api_key = self.meta_data["api_key"]
-
-        #create Object : OpenDataReader
-
-        #ignore 'module callable error'
+        #OpenDartReader class
         self.dart = OpenDartReader(self.api_key) # type: ignore
 
+        #test : dataframe   
+        # 당좌자산이랑 차입급이자급부채는 파싱하는 거 아님. 
+        # 비율항목 계산할 것 (기능사항 요구 명세서 선택데이터)
+        # 재무상태표 < 연결재무상대표, 손익계산서 < 포괄손익계산서(연결손익계산서) (테이블 따로)
+        #재무상태표에서 재고자산항목의 하위항목(상품,제품)은 없을 가능성도 있는데 이때 null처리 할 것.
+        # 회사 - 1.연도별 필수데이터 / 2.연도별 선택데이터 
 
 
-    #stock code might be a code (ex 005930), but also could be a stock name (ex 삼성전자)
-    def crawl_finstate_all(self,stock_code : str, year : int):
-        file_name = ""
-        if stock_code.isdigit() == True:
-            file_name = stock_code + self.dart.find_corp_code(stock_code)
-        else:
-            file_name = self.dart.find_corp_code(stock_code) + stock_code
-        file_name += str(year)
-        statement = self.dart.finstate_all(stock_code, year)
-        statement.to_excel(excel_writer = f'data/statement/{file_name}.xlsx')
 
+        #test = self.dart.finstate_all('농심', 2021)
+        # print(test.columns)
+        #test.to_excel(excel_writer = 'testdata/sk_test.xlsx')
+        #test.to_feather('testdata/test.feather')
+    
+    # "stock_list" items must be uniform as either ticker or name
+    #  ticker
+    def crawl_finstate_year(self,stock_list: list, year : int):
+        for stock in stock_list:
+            finstate = self.dart.finstate_all(stock,year)
+            finstate.to_feather(f'data/{year}{stock}.feather')
+
+            #정리해서 다시 엑셀파일저 저장 추후에 기능분리할 것
+            finstate.to_excel(excel_writer = f'testdata/{stock}.xlsx')
+
+
+
+#인터넷을 사용해서 긁어올 기업정보가 있을때 사용하는 클래스입니다.
+@singleton
+class CompanyInfoCrawler():
+    
+    def __init__(self) -> None:
+        self.reportCrawler = ReportCrawler()
+
+    def crawl_company_list(self):
+        print("hi")
+        pass
 
 def debug():
-    dataController = DataController()
-    reportCrawler = ReportCrawler(dataController)
-    reportCrawler.crawl_finstate_all("005930",2024)
+    reportCrawler =ReportCrawler()
+
+    test_list = ["005930","000660","373220","207940"]
     
+    company_info_crawler = CompanyInfoCrawler()
+
+    company_info_crawler.crawl_company_list()
+
+
+
 if __name__ == "__main__":
     debug()
-
 
