2161c1997ebeb53592cb69aee6b74728f466a366
jinuchoi <jinu010219@naver.com>
2025-11-28 14:52:23 +0900

    feat: embedding 기능 구현
    dataframe형식의 데이터를 embedding하는 기능을 구현하였
    습니다.
    rag.py 에는 청킹을 담당하는 CompanyDataChunking클래스와
    청킹된 데이터를 임베딩하고, 벡터 DB에 넣는 RAG 클래스가
    있습니다. (임베딩 기능만 작성완료) 임베딩할때의 청크 길이는
    현재 CHUNK_LENGTH = 1000 전후 지만. 실제 재무데이터를
    청킹하면 어떤 길이가 나올지 모르므로 변경될 가능성이 있습니다.
    그래도 가급적 청크 길이는 500~1500사이를 유지할 것입니다

--- ai/rag.py
+++ ai/rag.py
@@ -5,24 +5,28 @@ import tiktoken
 
 class ComapanyDataChunking:
 
-    def __init__(self,df:pd.DataFrame,company_name):
-        self.df = df
-        self.company_name = company_name
+    def __init__(self,df:pd.DataFrame,company_name,table_name):
+        #청크 토큰 길이
+        self.len = 0
+        #청크
+        self.chunk = ""
+
+        self.df_to_text_chunks(df,company_name,table_name)
+        self.len = self.token_counter(self.chunk)
 
     #api에 들어갈 토큰 길이를 카운팅합니다.
     #임베딩은 입력 토큰 길이가 500~1000로 제한할 것입니다.
     #token 측정은 모델별로 상이합니다.
-    def token_counter(self,text):
+    def token_counter(self,text) -> int:
         enc = tiktoken.get_encoding("cl100k_base")
         return len(enc.encode(text))
 
     #private
-    #아래 형식의 dataframe을 청크로 바꿔줍니다. token은 
+    #아래 형식의 dataframe을 str형의 청크로 바꿔줍니다. token은 
     #year    유동자산, 채권 ...
-    #2024Q4   4343   234 
+    #2024Q4   4343   234   이런 형태입니다.
     #회사 이름과, 테이블 이름이 string 인자로 들어갑니다.
     def df_to_text_chunks(self,df:pd.DataFrame,company_name:str,table_name:str):
-        chunks = []
         _type = "재무상태표"
         if table_name[-1] == "I":
             _type = "손익계산서"
@@ -45,12 +49,14 @@ class ComapanyDataChunking:
                     data_text += f" {col} : {val}"
             text += property_text
             text += data_text
-            chunks.append(text)
-        return chunks
+            text += "\n"
+            self.chunk += text
 
 
 @singleton
-class OpenAi:
+class RAG:
+
+    CHUNK_LENGTH = 1000
 
     #key.json 파일에서 api key를 불러오고, OpenAI 객체를 인스턴스 변수로 선언합니다.
     def __init__(self,test = False):
@@ -72,19 +78,36 @@ class OpenAi:
     #임베딩
     #db에서 불러온 데이터프레임을 리스트화 하고, 각 데이터프레임에 맞는 [회사이름,테이블이름] 리스트를
     #df_list 와 company_table_list로 집어넣습니다.
+    #한 번에 너무 많은 양을 df_list로 넣으면, 메모리 사용량이 커질 것 같습니다. 500개의 회사 단위로 넣는게 좋아보입니다.
     def get_embedding(self,df_list:pd.DataFrame,company_table_list:list,model : str = "small"):
+        #embedding 모델 설정 추후에 모델을 large로 바꿀 수도 있습니다.
         if model != "large" or model != "small":
             return
         model_name = "text-embedding-3-" + model
 
+        #임베딩 결과 저장 리스트
         embeddings = []
 
-
-        #수정 필요. 토큰길이 1000개 단위 정도로 잘라서 데이터 청킹후에, 아래 반복문 수정.
+        #청킹. 청킹 길이를 1000개 전후로 자릅니다.
+        chunked = []
+        last_chunk_size = 0
+        last_chunk = ""
         for idx in range(len(df_list)):
-            df = df_list[idx]; company = company_table_list[idx][0]; table = company_table_list[idx][1]
+            company = company_table_list[idx][0]; table = company_table_list[idx][1]
+            #청킹하고 1200개 미만이면 병합 1200개 넘으면 새로 
+            chunk = ComapanyDataChunking(df_list[idx],company,table)
+            if last_chunk_size + chunk.len >= RAG.CHUNK_LENGTH * 1.2:
+                chunked.append(last_chunk)
+                last_chunk = chunk
+            else:
+                last_chunk += chunk
+        #임베딩
+        
+        for chunk in chunked:
             emb = self.client.embeddings.create(
                 model = model_name,
-                input = df
-            )           
-
+                input = chunk
+            )
+            embeddings.append(emb.data[0].embedding)
+        
+        return embeddings
